49078ef0083b43d1eeb6c3f5fb6c51b1795b6f3d
Aman Dalmia <amandalmia18@gmail.com>
2018-09-26 22:20:27 +0530

    FIX: corrected bug in visualization

--- HeatmapGenerator.py
+++ HeatmapGenerator.py
@@ -38,12 +38,14 @@ class HeatmapGenerator ():
 
         modelCheckpoint = torch.load(pathModel)
         model.load_state_dict(modelCheckpoint['state_dict'])
-
-        self.model = model.module.densenet121.features
-        self.model.eval()
+        
+        self.feature_extractor = model.module.densenet121.features
+        self.feature_extractor.eval()
+        self.classifier = model.module.densenet121.classifier
         
         #---- Initialize the weights
-        self.weights = list(self.model.parameters())[-2]
+        self.weights = list(self.classifier.parameters())[-2].cpu().data.numpy()
+        self.bias = list(self.classifier.parameters())[-1].cpu().data.numpy()
 
         #---- Initialize the image transform - resize + normalize
         normalize = transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
@@ -56,7 +58,7 @@ class HeatmapGenerator ():
     
     #--------------------------------------------------------------------------------
      
-    def generate (self, pathImageFile, pathOutputFile, transCrop):
+    def generate (self, pathImageFile, pathOutputFile, classIndex, classLabel, transCrop):
         
         #---- Load image, transform, convert 
         imageData = Image.open(pathImageFile).convert('RGB')
@@ -65,23 +67,49 @@ class HeatmapGenerator ():
         
         input = torch.autograd.Variable(imageData)
         
-        self.model.cuda()
-        output = self.model(input.cuda())
+        self.feature_extractor.cuda()
+        output = self.feature_extractor(input.cuda())
+        output = output.squeeze(0)
+        output = output.cpu().data.numpy()
         
         #---- Generate heatmap
-        heatmap = None
-        for i in range (0, len(self.weights)):
-            map = output[0,i,:,:]
-            if i == 0: heatmap = self.weights[i] * map
-            else: heatmap += self.weights[i] * map
+        heatmap = np.zeros((output.shape[-2], output.shape[-1]))
+        for j in range(self.weights.shape[1]):
+            heatmap += self.weights[classIndex][j] * output[j]
+
+        heatmap += self.bias[classIndex]
+        
+        # code below is taken from https://github.com/jrzech/reproduce-chexnet  
+        heatmap=1 / (1 + np.exp(-heatmap))
+    
+        # calculated using frequentist approach
+        # based on prevalance of label throughout the dataset
+        label_baseline_probs={
+            'Atelectasis':0.103,
+            'Cardiomegaly':0.025,
+            'Effusion':0.119,
+            'Infiltration':0.177,
+            'Mass':0.051,
+            'Nodule':0.056,
+            'Pneumonia':0.012,
+            'Pneumothorax':0.047,
+            'Consolidation':0.042,
+            'Edema':0.021,
+            'Emphysema':0.022,
+            'Fibrosis':0.015,
+            'Pleural_Thickening':0.03,
+            'Hernia':0.002
+        }
+        
+        #normalize by baseline probabilities
+        heatmap = heatmap / label_baseline_probs[classLabel]
         
         #---- Blend original and heatmap 
-        npHeatmap = heatmap.cpu().data.numpy()
 
         imgOriginal = cv2.imread(pathImageFile, 1)
         imgOriginal = cv2.resize(imgOriginal, (transCrop, transCrop))
         
-        cam = npHeatmap / np.max(npHeatmap)
+        cam = (heatmap - np.min(heatmap)) / np.max(heatmap)
         cam = cv2.resize(cam, (transCrop, transCrop))
         heatmap = cv2.applyColorMap(np.uint8(255*cam), cv2.COLORMAP_JET)
               
@@ -91,7 +119,9 @@ class HeatmapGenerator ():
         
 #-------------------------------------------------------------------------------- 
 
-pathInputImage = 'test/00009285_000.png'
+pathInputImage = 'test/00011997_003.png'
+classIndex = 2
+classLabel = 'Effusion'
 pathOutputImage = 'test/heatmap.png'
 pathModel = 'models/m-25012018-123527.pth.tar'
 
@@ -101,4 +131,4 @@ nnClassCount = 14
 transCrop = 224
 
 h = HeatmapGenerator(pathModel, nnArchitecture, nnClassCount, transCrop)
-h.generate(pathInputImage, pathOutputImage, transCrop)
+h.generate(pathInputImage, pathOutputImage, classIndex, classLabel, transCrop)
